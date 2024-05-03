876b1d62d1930af3b39ecda3ccb2f694cc13e5c5
Martin Grap <rmsk2@gmx.de>
2024-05-03 20:02:57 +0200

    Begin to implement memory management

--- memory.asm
+++ memory.asm
@@ -2,6 +2,9 @@ NUM_PAGES_SIMPLE = 48
 NUM_PAGES_RAM_EXP = NUM_PAGES_SIMPLE + 32
 BLOCK_SIZE = 32
 PAGE_SIZE = 8192
+BLOCKS_PER_PAGE = PAGE_SIZE / BLOCK_SIZE
+
+PAGE_WINDOW = $A000
 
 FarPtr_t .struct
     lo   .byte 0
@@ -10,9 +13,9 @@ FarPtr_t .struct
 .endstruct
 
 MainMem_t .struct
-    addrPageMap   .word 0
-    numPages      .byte 0
-    numFreeBlocks .word 0
+    addrPageMap   .word PAGE_WINDOW
+    numPages      .byte NUM_PAGES_SIMPLE
+    numFreeBlocks .word NUM_PAGES_SIMPLE * BLOCKS_PER_PAGE
     ramExpFound   .byte 0
     numToPage     .fill NUM_PAGES_RAM_EXP
     pageToNum     .fill NUM_PAGES_RAM_EXP
@@ -24,12 +27,25 @@ memory .namespace
 MEM_STATE .dstruct MainMem_t
 
 init
-    lda #0
-    sta MEM_STATE.ramExpFound
+    ; assume no RAM expansion
+    stz MEM_STATE.ramExpFound
+    ; set default for number of pages
+    lda #NUM_PAGES_SIMPLE
+    sta MEM_STATE.numPages
+
     jsr setup.checkForRamExpansion
     bcs _noRamExp
+    ; record that RAM explansion is present
     inc MEM_STATE.ramExpFound
+    ; Increase number of available pages
+    lda #NUM_PAGES_RAM_EXP
+    sta MEM_STATE.numPages
 _noRamExp
+    lda MEM_STATE.numPages
+    sta $DE00
+    stz $DE01
+    #load16BitImmediate BLOCKS_PER_PAGE, $DE02
+    #move16Bit $DE10, MEM_STATE.numFreeBlocks
     rts
 
 .endnamespace
