5ad8b7997107dc248caf121e2c8fd9afc7a6accd
Martin Grap <rmsk2@gmx.de>
2024-05-10 00:45:45 +0200

    Add test case for alternative search in block allocation

---
+++ tests/altsearch.asm
@@ -0,0 +1,36 @@
+* = $0800
+.cpu "w65c02"
+
+HELP_PTR = $30
+
+jmp main
+
+STATE    .word memory.MEM_STATE         ; 3
+
+
+.include "zeropage.asm"
+.include "setup.asm"
+.include "arith16.asm"
+.include "memory.asm"
+
+
+main
+    jsr setup.mmu
+    jsr memory.init
+
+    ; allocate the first 512 blocks
+    #load16BitImmediate memory.MEM_STATE.pageMap, HELP_PTR
+    ldy #0
+    lda #$FF
+_loop
+    sta (HELP_PTR), y
+    iny
+    cpy #64
+    bne _loop
+    ; Adapt number of free blocks
+    #sub16BitImmediate 512, memory.MEM_STATE.numFreeBlocks
+
+    ; we are at block 0 on page 0. Try to allocate a new block. The next
+    ; free one is block 0 on page 2.
+    jsr memory.allocPtr
+    brk
---
+++ tests/altsearch.lua
@@ -0,0 +1,36 @@
+require("string")
+require (test_dir.."tools")
+
+-- This code tests whether allocPtr finds a new block 
+
+function arrange()
+    
+end
+
+
+function assert()
+    -- parse whole memory.MEM_STATE struct at the address stored at load_address + 3
+    local state = get_mem_state(de_ref(load_address + 3))
+    
+    -- Check whether allocation worked at all
+    if contains_flag("C") then
+        return false, string.format("An error occurred in iteration %d", get_xreg())
+    end
+
+    -- check that an additional block has been allocated
+    if state["numFreeBlocks"] ~= (state["numBlocks"] - 513) then
+        return false, string.format("There should have been 513 blocks allocated but there are %d", state["numFreeBlocks"])
+    end
+
+    -- check whether the first block on page 3 was allocated
+    if state["pageMap"][65] ~= 0x01 then
+        return false, string.format("Block map incorrect: %d", state["pageMap"][1])
+    end
+
+    -- check whether the number of trials has wrapped around to zero
+    if read_byte(0xA2) ~= 0 then
+        return false, string.format("Trial counter is not zero: %d", read_byte(0xA2))
+    end
+
+    return true, ""
+end
